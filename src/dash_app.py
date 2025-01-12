98493667a551211b299580a92cdbd01ab6bffe44
Neal Horner <thenealhorner@gmail.com>
2025-01-12 12:45:53 -0800

    changes for Vercel deployments

--- src/dash_app.py
+++ src/dash_app.py
@@ -2,10 +2,10 @@ import os
 from dash import Dash
 from flask_caching import Cache
 
-app = Dash()
+dash_app = Dash()
 
 cache = Cache(
-    app.server,
+    dash_app.server,
     config={
         "CACHE_TYPE": "filesystem",
         "CACHE_DIR": os.path.join(os.getcwd(), "cache"),
--- src/index.py
+++ src/index.py
@@ -1,8 +1,9 @@
-from app import app
+from dash_app import dash_app
 from layout import app_layout
 import callbacks  # noqa: F401
 
-app.layout = app_layout
+dash_app.layout = app_layout
+app = dash_app.server
 
 if __name__ == "__main__":
     app.run(debug=True)
