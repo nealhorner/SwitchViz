29fa618ec409b886db0a605513873b2e881f3d37
Neal Horner <thenealhorner@gmail.com>
2025-01-12 14:30:05 -0800

    plotly dash to next js conversion

--- src/index.py
+++
@@ -1,9 +0,0 @@
-from dash_app import dash_app
-from layout import app_layout
-import callbacks  # noqa: F401
-
-dash_app.layout = app_layout
-app = dash_app.server
-
-if __name__ == "__main__":
-    app.run(debug=True)
